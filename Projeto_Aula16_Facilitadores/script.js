0d4cb2f3908201bf1df4565c80277b7dfabe9a41
cristianjos <129961949+cristianjos1307@users.noreply.github.com>
2024-11-22 12:35:46 -0300

    Add files via upload

---
+++ Projeto_Aula16_Facilitadores/script.js
@@ -0,0 +1,19 @@
+  let nomes  =['Amir', 'Benir', 'Cemir'];
+  console.log(nomes.includes('Bemir'));
+  console.log(nomes.includes('AMIR'));
+
+  if(nomes.includes('Amir'))
+  {
+    console.log("Nome encontrado");
+  }
+  else
+  {
+    console.log("Nome não encontrado")
+  }
+
+  let aluno = 'Albano'
+  aluno.startsWith('alb');
+  aluno.startsWith('Alb');
+
+
+  console.log(alunos.endsWith('ano'));
